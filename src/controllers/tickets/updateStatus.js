f8abc9b3fb49b542d35015eab019894221446a3a
Amanda <amanda.asgm@gmail.com>
2025-08-17 17:46:30 -0300

    atualizando o status do ticket

--- src/controllers/tickets/updateStatus.js
+++ src/controllers/tickets/updateStatus.js
@@ -1,4 +1,7 @@
 // 35. atualizando um ticket com base na mudança de uma unica propriedade - STATUS
 export function updateStatus({ request, response, database }){
-  return response.end("ok")
+  const { id } = request.params
+  database.update("tickets", id, { status: "closed "})
+  
+  return response.writeHead(202).end()
 }
