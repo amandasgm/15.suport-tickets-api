583e93fd8269b9c1c30c0032a80e673a6a554839
Amanda <amanda.asgm@gmail.com>
2025-08-17 16:50:09 -0300

    rota para atualizar ticket

---
+++ src/controllers/tickets/update.js
@@ -0,0 +1,4 @@
+// 31. criando o controller de atualizar um ticket
+export function update({ request, response, database}){
+  response.end("ok")
+}
--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,6 +1,8 @@
 import { create } from "../controllers/tickets/create.js";
 // 22. importando o controller do metodo get e atualizando a linha 19
 import { index } from "../controllers/tickets/index.js";
+// 32. importando o controller do metodo put e atualizando a linha 27
+import { update } from "../controllers/tickets/update.js"
 
 // 7. criando rotas para cada tipo de requisição
 export const tickets = [
@@ -13,9 +15,16 @@ export const tickets = [
   },
 
   // 20. criando outros metodos
-    {
+  {
     method: "GET",
     path: "/tickets",
     controller: index,
+  },
+
+  // 31. criando metodo PUT
+  {
+    method: "PUT",
+    path: "/tickets/:id",
+    controller: update,
   },
 ]
